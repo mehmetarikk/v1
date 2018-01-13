66652c40ac83e2104d3b50e2b47bcf25a169fb45
mehmetarikk <34798991+mehmetarikk@users.noreply.github.com>
2018-01-13 23:39:20 +0300

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+BURAYA EKLEDİM BUNU
+
 module.exports = function (config) {
   config.set({
     basePath: '',
